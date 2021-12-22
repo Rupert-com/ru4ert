9cd71dddc93ebd7982d0507c7a8d823abbd12554
Rupert Bogensperger <rupertbogensperger21@gmail.com>
2021-12-22 09:06:46 +0100

    added express to firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,10 @@
 import * as functions from 'firebase-functions'
+import express = require('express')
+import path = require('path')
+const app = express()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'))
 })
 
-export const index = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase index and hot!')
-})
-// import * as functions from 'firebase-functions'
-// import * as util from '../../shared/util'
-
-// exports.addMessage = functions.https.onRequest((request, response) => {
-//   util.helloWorld()
-//   response.send('Hello from Firebase!')
-// })
+export const index = functions.https.onRequest(app)
