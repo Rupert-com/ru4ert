e4c3ad82d56679d7ed38cfff41116f9f36c0a6ab
Rupert Bogensperger <rupertbogensperger21@gmail.com>
2022-07-04 21:20:20 +0200

    added structured data

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -194,6 +194,144 @@ export const App: FunctionComponent<INavProps> = ({ darkmode, updateDarkmode, ..
               content="Rupert Bogensperger | cross platform app development on mobile devices and enterprise. JavaScript, TypeScript, HTML and CSS are my main techniques to develop for web. Further, I use programming languages like python, C# or Kotlin that targets the JVM, Android, JavaScript and Native."
             />
             <meta property="og:type" content="website" />
+
+            <script type="application/ld+json">
+              {JSON.stringify({
+                '@context': 'https://schema.org',
+                '@type': 'BreadcrumbList',
+                itemListElement: pages.map(({ name, path }, index) => ({
+                  '@type': 'ListItem',
+                  position: 1,
+                  name: name,
+                  item: `https://ru4ert.com${path}`,
+                })),
+              })}
+            </script>
+            <script type="application/ld+json">
+              {JSON.stringify({
+                '@context': 'http://www.schema.org',
+                '@type': 'Person',
+                name: 'Rupert Bogensperger',
+                nationality: {
+                  '@type': 'Country',
+                  address: 'Austria',
+                },
+                givenName: 'Rupert',
+                familyName: 'Bogensperger',
+                jobTitle: 'Head of IT',
+                url: 'https://ru4ert.com/',
+                gender: 'male',
+                address: {
+                  '@type': 'PostalAddress',
+                  streetAddress: 'Nusswaldgasse, 23-25',
+                  postOfficeBoxNumber: '1',
+                  addressLocality: 'Vienna, Austria',
+                  addressRegion: 'Vienna',
+                  postalCode: '1190',
+                  addressCountry: 'AT',
+                },
+                email: 'me@ru4ert.com',
+                telephone: '+436601873938',
+                birthDate: '2001-09-23',
+                sameAs: ['https://www.instagram.com/ru4ert/', 'https://github.com/Rupert-com', 'https://www.thingiverse.com/rupiert/designs', 'https://at.linkedin.com/in/ru4ert', 'https://wa.me/message/RSEKN2ONZFS7E1', 'https://stackoverflow.com/users/11123801/ru4ert'],
+                alumniOf: [
+                  {
+                    '@type': 'EducationalOrganization',
+                    address: {
+                      '@type': 'PostalAddress',
+                      addressCountry: 'AT',
+                      addressLocality: 'Vienna, Austria',
+                      addressRegion: 'Vienna',
+                      postalCode: '1200',
+                      streetAddress: 'Wexstraße 19-23',
+                    },
+                    name: 'TGM - Technologisches Gewerbemuseum',
+                  },
+                  {
+                    '@type': 'EducationalOrganization',
+                    address: {
+                      '@type': 'PostalAddress',
+                      addressCountry: 'AT',
+                      addressLocality: 'Salzburg, Austria',
+                      addressRegion: 'Salzburg',
+                      postalCode: '5760',
+                      streetAddress: 'Almerstraße 33',
+                    },
+                    name: 'HTL Saalfelden',
+                  },
+                  {
+                    '@type': 'EducationalOrganization',
+                    address: {
+                      '@type': 'PostalAddress',
+                      addressCountry: 'AT',
+                      addressLocality: 'Salzburg, Austria',
+                      addressRegion: 'Salzburg',
+                      postalCode: '5580',
+                      postOfficeBoxNumber: 'A',
+
+                      streetAddress: 'Lasabergweg 12',
+                    },
+                    name: 'Bundesgymnasium Tamsweg',
+                  },
+                ],
+                knowsLanguage: [
+                  {
+                    '@type': 'Language',
+                    name: 'English',
+                    alternateName: 'en',
+                  },
+                  {
+                    '@type': 'Language',
+                    name: 'German',
+                    alternateName: 'de',
+                  },
+                ],
+                worksFor: {
+                  '@type': 'Organization',
+                  address: {
+                    '@type': 'PostalAddress',
+                    addressCountry: 'AT',
+                    addressLocality: 'Vienna, Austria',
+                    addressRegion: 'Vienna',
+                    postalCode: '1020 ',
+                    streetAddress: 'Franz Hochedlingergasse 26/27',
+                  },
+                  funder: {
+                    '@type': 'Person',
+                    name: 'Aurelius Nöll',
+                    givenName: 'Aurelius',
+                    familyName: 'Nöll',
+                    jobTitle: 'Founder',
+                  },
+                  contactPoint: [{ '@type': 'ContactPoint', email: 'hello@thesixthedition.com', contactType: 'customer service', availableLanguage: ['English', 'German'] }],
+                  email: 'hello@thesixthedition.com',
+                  name: 'Sixth Edition GmbH',
+                },
+                brand: {
+                  '@type': 'Organization',
+                  url: 'https://ru4ert.com',
+                  email: 'rupertbogensperger21@gmail.com',
+                  knowsLanguage: [
+                    {
+                      '@type': 'Language',
+                      name: 'English',
+                      alternateName: 'en',
+                    },
+                    {
+                      '@type': 'Language',
+                      name: 'German',
+                      alternateName: 'de',
+                    },
+                  ],
+                  logo: 'https://ru4ert.com/ru4ert.png',
+                  brand: {
+                    '@type': 'Brand',
+                    name: 'ru4ert',
+                    logo: 'https://www.gravatar.com/avatar/a98495ef27eacd246572b931ac8dd6dc?s=180&d=mp&r=g',
+                  },
+                },
+              })}
+            </script>
           </Helmet>
           <Routes>
             <Route path="/" element={<About />} />
